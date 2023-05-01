95cfc05e6412cddea94e998c885b7815d11452bd
wonderhaldev <119766866+wonderhaldev@users.noreply.github.com>
2023-05-02 09:31:51 +1200

    change brand colours

--- src/config/configBranding.js
+++ src/config/configBranding.js
@@ -15,7 +15,7 @@ import twitterImage from '../assets/biketribe-twitter-sharing-600x314.jpg';
 // This is saved as CSS Property: --marketplaceColor in src/app.js
 // Also --marketplaceColorDark and --marketplaceColorLight are generated from this one
 // by adding +/- 10% to lightness.
-export const marketplaceColor = '#7c3aed';
+export const marketplaceColor = '#8cfff3';
 
 // Logo is used in Topbar on mobile and desktop, where height is the limiting factor.
 // Therefore, we strongly suggest that your image file for desktop logo is in landscape!
